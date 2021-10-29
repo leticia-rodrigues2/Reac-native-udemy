51ce1fbb481a5a8e8d6a09c250fdf53f1c6769a4
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-10-29 11:30:49 -0300

    Componete 2

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,8 +1,4 @@
 import React from "react"
 import {Text} from 'react-native'
 
-function App (){
-  return <Text> Primeiro componente</Text>
-}
-
-export default App
+export default () => <Text> Primeiro componente !!! </Text>
