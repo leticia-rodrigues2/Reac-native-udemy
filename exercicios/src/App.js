3f4235b66350a87ae080dd671bf701ed1af5746b
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-10-29 13:00:24 -0300

    Sistema de Módulos do ECMAScript

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,15 @@
 import React from "react"
 import {View} from 'react-native'
+
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 import Primeiro  from './components/Primeiro'
 
 export default () =>
 (
   <View> 
+    <CompPadrao />
+    <Comp1 />
+    <Comp2 />
     <Primeiro />
   </View> 
 )
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import { Text } from 'react-native';
+
+export default function Comp (){
+  return <Text> Comp #Oficial</Text>
+}
+
+export function Comp1 (){
+  return <Text> Comp #01 </Text>
+}
+
+export function Comp2 (){
+  return <Text> Comp #02</Text>
+}
