ed9ab3c8d28ec03f1da58ced39132ad4a764c923
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-11-16 09:14:40 -0300

    Componente de Classe: Estado aula 51

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,14 +1,53 @@
 import React , {Component} from "react";
-import { Text } from "react-native";
+import { Text, TextInput } from "react-native";
 import Estilo from '../estilo'
 
+
 export default class Mega extends Component {
+  
+    state = {
+      qtdeNumeros:this.props.qtdeNumeros 
+    }
+
+    alterarQtdeNumeros (qtde){
+      this.setState({qtdeNumeros: qtde})
+    }
+  
+
   render(){
     return(
+      <>
       <Text style = {Estilo.txtG}>
         Gerador de Mega-Sena 
-        {this.props.qtdeNumeros}
+        {this.state.qtdeNumeros}
       </Text>
+      <TextInput
+          placeholder = "Quantide de Números"
+          value = {this.setState.qtdeNumeros}
+         onChangeText={this.alterarQtdeNumeros}
+      />
+      </>
     )
   }
 }
+
+
+//USANDO CONSTRUTOR
+
+//   constructor (props){
+//     super(props)
+  
+//     this.state = {
+//       qtdeNumeros:props.qtdeNumeros + 1000
+//     }
+//   }
+
+//   render(){
+//     return(
+//       <Text style = {Estilo.txtG}>
+//         Gerador de Mega-Sena 
+//         {this.state.qtdeNumeros}
+//       </Text>
+//     )
+//   }
+// }
